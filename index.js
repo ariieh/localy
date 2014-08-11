5e8ece61e016e62beaeac2284229e5c22174bc7e
Beth Stephens <bstephens129@new-host-4.home>
2014-08-11 14:00:28 -0400

    Set up vendor, JS library routes

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+/* Router */
+//Root
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-app.get('/vendor/jquery.js', function(req, res){
-  res.sendfile('vendor/jquery.js');
-});
+//Vendor
+app.get('/vendor/jquery.js', function(req, res){ res.sendfile('vendor/jquery.js'); });
 
+//JavaScript library
+app.get('/lib/geolocation.js', function(req, res){ res.sendfile('lib/geolocation.js'); });
+
+/* IO connections */
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
+/* Server */
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
