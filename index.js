a4f73647fb9160e54a4f0e8126ff5bed30068767
Arieh Smith <arieh.smith@gmail.com>
2014-08-13 14:20:36 -0400

    Add user count

--- index.js
+++ index.js
@@ -2,37 +2,46 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var size = 0;
 
 var serverMarkerData = {};
 
 /* Router */
-//Root
-app.get('/', function(req, res){ res.sendfile('index.html'); });
+	//Root
+	app.get('/', function(req, res){ res.sendfile('index.html'); });
 
-//Vendor
-app.get('/vendor/:file', function(req, res){ res.sendfile('vendor/' + req.params.file); });
-app.get('/vendor/images/:image', function(req, res){ res.sendfile('vendor/images/' + req.params.image); });
+	//Vendor
+	app.get('/vendor/:file', function(req, res){ res.sendfile('vendor/' + req.params.file); });
+	app.get('/vendor/images/:image', function(req, res){ res.sendfile('vendor/images/' + req.params.image); });
 
-//JavaScript library
-app.get('/lib/:file', function(req, res){ res.sendfile('lib/' + req.params.file); });
+	//JavaScript library
+	app.get('/lib/:file', function(req, res){ res.sendfile('lib/' + req.params.file); });
 
-//Stylesheets
-app.get('/stylesheets/:file', function(req, res){ res.sendfile('stylesheets/' + req.params.file); });
+	//Stylesheets
+	app.get('/stylesheets/:file', function(req, res){ res.sendfile('stylesheets/' + req.params.file); });
 
 /* IO connections */
-io.on('connection', function(socket){
+	io.on('connection', function(socket){
+		
+		socket.on('load map', function(userID){
+	    io.to(userID).emit('load map', serverMarkerData);
+			size += 1;
+			io.emit('user count', size)
+		});
 	
-	socket.on('load map', function(userID){
-    io.to(userID).emit('load map', serverMarkerData);
+	  socket.on('load marker', function(coords, name, userID){
+			serverMarkerData[userID] = [coords, name];
+	    io.emit('load marker', coords, name, userID);
+	  });
+		
+		socket.on('disconnect', function(){
+			size -= 1;
+			io.emit('user count', size)
+		});
+		
 	});
 	
-  socket.on('load marker', function(coords, name, userID){
-		serverMarkerData[userID] = [coords, name];
-    io.emit('load marker', coords, name, userID);
-  });
-});
-
 /* Server */
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+	http.listen(3000, function(){
+	  console.log('listening on *:3000');
+	});
