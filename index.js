126d03b15cfe2bc27de2205bb4d8e5969028e3b1
Arieh Smith <arieh.smith@gmail.com>
2014-12-27 19:27:55 -0500

    Remove test table pg code

--- index.js
+++ index.js
@@ -26,18 +26,6 @@ var router = exports.router = require('./lib/server/router.js');
 /* Database */
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-});
-
 /* IO connections */
 io.on('connection', function(socket) {
   io.to(socket.id).emit('connected');
