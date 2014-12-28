985d0263c75c236445fc2a6e38907fae328b4a47
Arieh Smith <arieh.smith@gmail.com>
2014-12-27 19:35:04 -0500

    Use heroku db url

--- knexfile.js
+++ knexfile.js
@@ -31,10 +31,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'localy_production'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
