0b415fa52e75c400be46cd4dad85e0a4e1a8fdad
Arieh Smith <arieh.smith@gmail.com>
2015-01-03 21:59:46 -0500

    Fix chatroom button styling and switching bugs

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.seed = function(knex, Promise) {
-  return knex('users').insert({
+  knex('users').insert({
     username: 'arieh',
     socket_id: '1234',
     latitude: 0.71068852446,
